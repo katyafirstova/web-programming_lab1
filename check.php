a2e49bb4da9c2227667d685b8d1370cfb588f68e
kat <firstovakatya42@gmail.com>
2022-09-22 14:30:40 +0300

    change styles

--- check.php
+++ check.php
@@ -13,41 +13,45 @@ $y = $_GET['y'];
 $r = $_GET['r'];
 
 $check = false;
+$fail = "Некорректное значение переменных";
 
 
-if (is_numeric($x) && is_numeric($r) && ($y > -5 && $y < 3)) $check = true;
+
+if (is_numeric($x) && is_numeric($r) && is_numeric($y)) $check = true;
+elseif (!is_numeric($x) || $r = null || !($y > -5 && $y < 3)) die($fail);
 
 
-if (($x < 1.5 && $r >= 0 && $r <= 1.5 && $y >= -3 && $y <= 0) || ($r >= 0 && $r <= 1.5 && $x >= 0
-        && $x <= 3 && $y >= 0 && $y <= 1.5) || ($r >= 0 && $r <= 3 && $x >= 0 && $x <= 3 &&
-        $y >= -3 && $y <= 0)) {
-    $res = "да";
-} else {
-    $res = "нет";
+
+if($check) {
+    function checkCoords($x, $y, $r): bool
+    {
+        if (($x < 1.5 && $r >= 0 && $r <= 1.5 && $y >= -3 && $y <= 0) || ($r >= 0 && $r <= 1.5 && $x >= 0
+                && $x <= 3 && $y >= 0 && $y <= 1.5) || ($r >= 0 && $r <= 3 && $x >= 0 && $x <= 3 &&
+                $y >= -3 && $y <= 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 
-    if ($check) {
-        // Сохранение в сессию
-//        $result = array($x, $y, $r, $res, $currentTime, $executionTime);
-        $result = array($x.";".$y.";".$r, "да", $currentTime, $executionTime);
+    if (checkCoords($x, $y, $r)) {
+        $result = array($x . ";" . $y . ";" . $r, "да", $currentTime, $executionTime);
 
         if (!isset($_SESSION['results'])) {
             $_SESSION['results'] = array();
         }
         array_push($result);
-        echo $x . "; " . $y . "; " . $r . "#да#" . $currentTime. "#" . $executionTime;
-    }
-    else {
-        // Сохранение в сессию
-        $result = array($x.";".$y.";".$r, "нет", $currentTime, $executionTime);
+        echo $x . "; " . $y . "; " . $r . "#да#" . $currentTime . "#" . $executionTime;
+    } else {
+        $result = array($x . ";" . $y . ";" . $r, "нет", $currentTime, $executionTime);
 
         if (!isset($_SESSION['results'])) {
             $_SESSION['results'] = array();
         }
         array_push($result);
-        echo $x . "; " . $y . "; " . $r . "#нет#" . $currentTime. "#" . $executionTime;
+        echo $x . "; " . $y . "; " . $r . "#нет#" . $currentTime . "#" . $executionTime;
     }
-
 
 
--- index.php
+++ index.php
@@ -28,47 +28,43 @@
     </h2>
 </header>
 
-
-<table cos>
+<table>
     <tr>
         <td>
-            <figure align="right">
-                <img src="./img/img.png" alt="img">
-            </figure>
-
             <form id="form">
                 <h3>
                     X:
                 </h3>
-                    <label class="required" for="button" data-required="Выберите X"></label>
-                    <input type="button" name="x" class="button1" value="-5">
-                    <input type="button" name="x" class="button2" value="-4">
-                    <input type="button" name="x" class="button3" value="-3">
-                    <br>
-                    <input type="button" name="x" class="button1" value="-2">
-                    <input type="button" name="x" class="button2" value="-1">
-                    <input type="button" name="x" class="button3" value="0">
-                    <br>
-                    <input type="button" name="x" class="button1" value="1">
-                    <input type="button" name="x" class="button2" value="2">
-                    <input type="button" name="x" class="button3" value="3">
-                    <br><br>
+                <br><br>
+                <label class="required" for="button" data-required="Выберите X"></label>
+                <input type="button" name="x" class="button1" value="-5">
+                <input type="button" name="x" class="button2" value="-4">
+                <input type="button" name="x" class="button3" value="-3">
+                <br>
+                <input type="button" name="x" class="button1" value="-2">
+                <input type="button" name="x" class="button2" value="-1">
+                <input type="button" name="x" class="button3" value="0">
+                <br>
+                <input type="button" name="x" class="button1" value="1">
+                <input type="button" name="x" class="button2" value="2">
+                <input type="button" name="x" class="button3" value="3">
                 <div class="error" id="xError"></div>
+                <figure>
+                    <img src="./img/img.png" alt="img">
+                </figure>
                 <h3>
                     Y:
                 </h3>
-
-                    <div class="form-control">
-                        <label for="y"></label>
-
-                        <input name="y" id="y" min="-3" max="5" class="inputY"
-                               placeholder="Введите значение от -3 до 5">
-                        <i class="fas fa-check-circle"></i>
-                        <i class="fas fa-exclamation-circle"></i>
-                        <small>Error message</small>
-                        <span class="highlight"></span>
-                        <span class="bar"></span>
-                        <span class="yError" aria-live="polite"></span>
+                <div class="form-control">
+                    <label for="y"></label>
+                    <input name="y" id="y" min="-3" max="5" class="inputY"
+                           placeholder="Введите значение от -3 до 5">
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                    <small>Error message</small>
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <span class="yError" aria-live="polite"></span>
 
                     <br><br><br><br><br>
                 </div>
@@ -76,50 +72,57 @@
                 <h3>
                     R:
                 </h3>
-                    <label class="required" for="select" data-required="Выберите R"></label>
-                    <select class="selectR" name="select" id="select">
-                        <option selected="" name="R" disabled="disabled" value="">R</option>
-                        <option value="1">1</option>
-                        <option value="1.5">1.5</option>
-                        <option value="2">2</option>
-                        <option value="2.5">2.5</option>
-                        <option value="3">3</option>
-                    </select>
+
+                <label class="required" for="select" data-required="Выберите R"></label>
+                <select class="selectR" name="select" id="select">
+                    <option selected="" name="R" disabled="disabled" value="">R</option>
+                    <option value="1">1</option>
+                    <option value="1.5">1.5</option>
+                    <option value="2">2</option>
+                    <option value="2.5">2.5</option>
+                    <option value="3">3</option>
+                </select>
                 <input type="submit" id="submit" class="btn" value="Submit">
                 <div id="response"></div>
+
+
+
+
+
             </form>
             <script src="validation.js"></script>
         </td>
     </tr>
 
 
-<tr id="tr-result">
-    <td>
-        <table id="result-table">
-            <thead>
-            <tr>
-                <th scope="col">XYR</th>
-                <th scope="col">Результат</th>
-                <th scope="col">Current Time</th>
-                <th scope="col">Computation Time</th>
-            </tr>
-            </thead>
-            <tbody id="results_table_body">
+    <tr id="tr-result">
+        <td>
+            <table id="result-table">
+                <thead>
+                <tr>
+                    <th scope="col">XYR</th>
+                    <th scope="col">Result</th>
+                    <th scope="col">Current Time</th>
+                    <th scope="col">Computation Time</th>
+                </tr>
+                </thead>
+                <tbody id="results_table_body">
 
-            <?php
-            if (isset($_SESSION['results'])) {
-                foreach ($_SESSION['results'] as $result) { ?>
-                    <tr>
-                        <th><?php echo $result[0] ?></th>
-                        <th><?php echo $result[1] ?></th>
-                        <th><?php echo $result[2] ?></th>
-                        <th><?php echo $result[3] ?></th>
-                    </tr>
+                <?php
+                if (isset($_SESSION['results'])) {
+                    foreach ($_SESSION['results'] as $result) { ?>
+                        <tr>
+                            <th><?php echo $result[0] ?></th>
+                            <th><?php echo $result[1] ?></th>
+                            <th><?php echo $result[2] ?></th>
+                            <th><?php echo $result[3] ?></th>
+                        </tr>
 
-                <?php }} ?>
-        </table>
-    </td>
-</tr>
+                    <?php }
+                } ?>
+            </table>
+        </td>
+    </tr>
 </table>
 </body>
 </html>
